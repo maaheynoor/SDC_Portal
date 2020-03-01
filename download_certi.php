a4aa1e8b8de593eccb64c9955ea9c7d957c764a5
maaheynoor <50229220+maaheynoor@users.noreply.github.com>
2020-03-01 16:59:39 +0530

    code for download certificate(pdf)

--- download_certi.php
+++ download_certi.php
@@ -1,16 +1,24 @@
 <?php
+//to preview certificate and download
 session_start();
 require 'dbconfig/config.php';
 if(isset($_GET['Int_Certificate']))
 {
   $intcerti=$_GET['Int_Certificate'];
   $file='intp_certificate/'.$intcerti;
-  $filename=basename($file);
-  if(file_put_contents( $filename,file_get_contents($file))) {
-    echo "File downloaded successfully";
+//   $filename=basename($file);
+//   if(file_put_contents( $filename,file_get_contents($file))) {
+//     echo "File downloaded successfully";
+// }
+// else {
+//     echo "File downloading failed.";
+// }
+  header('Content-type:application/pdf');
+  header('Content-Disposition:inline;filename="'.$file.'"');
+  header('Content-Tansfer-Encoding:binary');
+  header('Accept-Ranges:bytes');
+  @readfile($file);
 }
-else {
-    echo "File downloading failed.";
-}
-}
+
+
 ?>
